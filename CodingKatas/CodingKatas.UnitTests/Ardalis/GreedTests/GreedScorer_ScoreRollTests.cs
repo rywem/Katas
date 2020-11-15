785138e0ce53837b8e88ba1c41be1f7722674382
Ryan <cybervoid41@gmail.com>
2020-11-15 11:00:48 -0800

    Greed problem initial commit

---
+++ CodingKatas/CodingKatas.UnitTests/Ardalis/GreedTests/GreedScorer_ScoreRollTests.cs
@@ -0,0 +1,51 @@
+﻿using CodingKatas.Ardalis.Greed;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+namespace CodingKatas.UnitTests.Ardalis.GreedTests
+{
+    public class GreedScorer_ScoreRollTests
+    {
+        [Fact]
+        public void Returns100GivenSingleOne()
+        {
+            var scorer = new GreedScorer();
+
+            var result = scorer.ScoreRoll(1);
+            result.Should().Be(100);
+        }
+
+        [Fact]
+        public void Returns200GivenTwoOnes()
+        {
+            var scorer = new GreedScorer();
+
+            var result = scorer.ScoreRoll(1, 1);            
+            result.Should().Be(200);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void Returns0GivenSingleWorthlessValue(int val)
+        {
+            var scorer = new GreedScorer();
+
+            var result = scorer.ScoreRoll(val);
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Return50GivenSingleFive()
+        {
+            var scorer = new GreedScorer();
+
+            var result = scorer.ScoreRoll(5);
+            result.Should().Be(50);
+        }
+    }
+}
---
+++ CodingKatas/CodingKatas/Ardalis/Greed/GreedScorer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Linq;
+namespace CodingKatas.Ardalis.Greed
+{
+    public class GreedScorer
+    {
+        public int ScoreRoll( params int[] dieValues )
+        {
+            if ( !dieValues.Any(i => i == 1) )
+                return 0;
+            if ( dieValues.Count(i => i == 1) == 1 )
+            {
+                return 100;
+            }
+            return 200;
+        }
+    }
+
+    public class Dice : IEquatable<Dice>
+    {
+        public int CurrentRoll { get; set; }
+        private Random _random;
+        public Dice()
+        {
+
+        }
+
+        public Dice(Random random)
+        {
+            _random = random;
+        }
+
+        public int Roll()
+        {
+            Random random = new Random();
+            CurrentRoll = random.Next(1, 6);
+            return CurrentRoll;
+        }
+
+        public bool Equals( [AllowNull] Dice other )
+        {
+            if ( other == null )
+                return false;
+
+            return this.CurrentRoll == other.CurrentRoll;
+        }
+    }
+}
