b183de9d10acc766438ca304ec7ffd1c35efec90
Ryan <cybervoid41@gmail.com>
2022-05-14 08:03:00 -0600

    Simple Factory pattern

--- DesignPatterns/Factory/FactoryPattern/FactoryPatternConsole/Point.cs
+++ DesignPatterns/Factory/FactoryPattern/FactoryPatternConsole/Point.cs
@@ -3,8 +3,16 @@ namespace FactoryPatternConsole
 {
 	public class Point
 	{
-		public Point()
+		// Factory method
+		public static Point NewCartesianPoint(double x, double y)
+        {
+			return new Point(x, y);
+        }
+		private double x, y;
+		private Point(double x, double y)
 		{
+			this.x = x;
+			this.y = y;
 		}
 	}
 }
