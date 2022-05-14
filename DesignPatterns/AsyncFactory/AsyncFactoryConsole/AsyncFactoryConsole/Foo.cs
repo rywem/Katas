665b12c4699b4949ef42047be370978564fa6041
Ryan <cybervoid41@gmail.com>
2022-05-14 08:09:33 -0600

    Async Factory created

---
+++ DesignPatterns/AsyncFactory/AsyncFactoryConsole/AsyncFactoryConsole/Foo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncFactoryConsole
+{
+    public class Foo
+    {
+
+        private Foo()
+        {
+            //private
+        }
+        private async Task<Foo> InitAsync()
+        {
+            // private, this is where we do the async work
+            await Task.Delay(1000); 
+            return this;
+        }
+
+        // This is our public factory method.
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+}
