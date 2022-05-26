c462c4126e9e745b26f00d0bc4431db1cae2ab09
Ryan <ryan.wemmer@gmail.com>
2022-05-26 09:05:03 -0600

    FactoryPatternTests added

---
+++ DesignPatterns/Factory/FactoryPattern/FactoryPatternTests/Examples/TrackingThemeFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using FactoryPatternConsole.Examples;
+namespace FactoryPatternTests.Examples
+{
+	public class TrackingThemeFactoryTests
+	{
+		[Fact]
+		public void AssertFactoryReturnsDarkTheme()
+        {
+			var factory = new TrackingThemeFactory();
+			var theme = factory.CreateTheme(true);
+			var bgColor = "dark gray";
+			var textColor = "white";
+			Assert.Equal(bgColor, theme.BackgroundColor);
+			Assert.Equal(textColor, theme.TextColor);
+        }
+	}
+}
+
