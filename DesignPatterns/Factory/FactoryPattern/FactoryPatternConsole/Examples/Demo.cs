3c7d0d195c9405c6ab268069715a054f613ff5b4
Ryan <ryan.wemmer@gmail.com>
2022-05-26 08:55:45 -0600

    TrackingThemeFactory added to example

---
+++ DesignPatterns/Factory/FactoryPattern/FactoryPatternConsole/Examples/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FactoryPatternConsole.Examples
+{
+	public class Demo
+	{
+		public void Run()
+        {
+			var factory = new TrackingThemeFactory();
+			var theme1 = factory.CreateTheme(false);
+			var theme2 = factory.CreateTheme(true);
+
+			Console.WriteLine(factory.Info);
+        }
+	}
+}
+
--- DesignPatterns/Factory/FactoryPattern/FactoryPatternConsole/Program.cs
+++ DesignPatterns/Factory/FactoryPattern/FactoryPatternConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using FactoryPatternConsole;
+using FactoryPatternConsole.Examples;
 
 Console.WriteLine("Hello, World!");
 
-var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2;
-Console.WriteLine(point);
+//var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2)s;
+//Console.WriteLine(point);
+Demo demo = new Demo();
+demo.Run();
