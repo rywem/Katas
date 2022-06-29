db30e9e3e0671af5294fbbb9a065c5baad7232d4
Ryan <cybervoid41@gmail.com>
2022-06-29 09:35:41 -0600

    AbstractFactory added, but this violates the OCP with the enum AvailableDrink

---
+++ DesignPatterns/AbstractFactory/AbstractFactory/HotDrink.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace AbstractFactory
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("This tea is nice, but I'd prefer it with milk.");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            WriteLine("This coffee is sensational!");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon, enjoy");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            WriteLine($"Grind some beans, boil water, pour {amount} ml, add cream and sugar, enjoy");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Coffee,
+            Tea
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType("AbstractFactory." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+
+                factories.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return factories[drink].Prepare(amount);
+        }
+
+    }
+}
+
+
--- DesignPatterns/AbstractFactory/AbstractFactory/Program.cs
+++ DesignPatterns/AbstractFactory/AbstractFactory/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AbstractFactory;
+
+var machine = new HotDrinkMachine();
+
+var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+drink.Consume();
